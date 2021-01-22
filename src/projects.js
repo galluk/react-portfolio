9a9fc3d37835f8bf999a5873ff53650f81e288bc
HomeAlone <luke.gallagher.cst@gmail.com>
2021-01-22 18:06:04 +1100

    portfolio changed to be build by React

---
+++ src/projects.js
@@ -0,0 +1,63 @@
+export const projectsData = [
+  {
+    "id": 1,
+    "name": "A Burger Eating App",
+    "image": "/images/burger.jpg",
+    "blurb": "A full stack app using MySQL, Node, Express, Handlebars and ORM.",
+    "appText": "Eat what you want when you want with the app ",
+    "appLink":"https://tranquil-bastion-70586.herokuapp.com/",
+    "gitText" :"View the repository on my Burger ",
+    "gitLink" :"https://github.com/galluk/burger"
+  },
+  {
+    "id": 2,
+    "name": "A Workday Scheduler",
+    "image": "/images/scheduler.jpg",
+    "blurb": "An easy to use Scheduler to plan your current day.",
+    "appText": "See what you can do to organise your day ",
+    "appLink":"https://galluk.github.io/Workday-Scheduler/index.html",
+    "gitText" :"View the repository on my Scheduler ",
+    "gitLink" :"https://github.com/galluk/Workday-Scheduler"
+  },
+  {
+    "id": 3,
+    "name": "A Weather Dashboard",
+    "image": "/images/weather-dash.jpg",
+    "blurb": "Want to know what the weather's like anywhere in the world?",
+    "appText": "Find out ",
+    "appLink":"https://galluk.github.io/weather-dashboard/index.html",
+    "gitText" :"View the repository on my weather ",
+    "gitLink" :"https://github.com/galluk/weather-dashboard"
+  },
+  {
+    "id": 4,
+    "name": "Book a Tutor",
+    "image": "/images/book-a-tutor.jpg",
+    "blurb": "If you need help with your studies, then you need to check out our group project!",
+    "appText": "Eat what you want when you want with the app ",
+    "appLink":"https://book-a-tutor.herokuapp.com/",
+    "gitText" :"View the repository on the project ",
+    "gitLink" :"https://github.com/Epanastatis2020/book-a-tutor"
+  },
+  {
+    "id": 5,
+    "name": "Budget Tracker",
+    "image": "/images/budget-tracker.jpg",
+    "blurb": "Are you spending too much?",
+    "appText": "Keep an eye on your income and expenses ",
+    "appLink":"https://immense-beyond-23497.herokuapp.com/",
+    "gitText" :"View the repository on the project ",
+    "gitLink" :"https://github.com/galluk/budget-tracker"
+  },
+  {
+    "id": 5,
+    "name": "Workout Tracker",
+    "image": "/images/workout.jpg",
+    "blurb": "Are you looking to improve your fitness during COVID?",
+    "appText": "Plan and view your workouts ",
+    "appLink":"https://limitless-ravine-81223.herokuapp.com/",
+    "gitText" :"View the repository on the project ",
+    "gitLink" :"https://github.com/galluk/workout-tracker"
+  }
+]
+
